97d18a6eab1d5fdfb7edc4262a07aeb6747a3068
cuizhichao <cuizc@primeton.com>
2019-11-04 10:23:31 +0800

    添加简单示例

--- 0.Data_cleaning.py
+++ 0.Data_cleaning.py
@@ -3,3 +3,17 @@
 Created on 2019-11-02 20:55
 @author: cuizc
 """
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+power = pd.read_csv('household_power_consumption_1000.txt', sep=";")
+
+print(power.head())
+print(power.info())
+# 统计值，包括平均数，标准差，中位数，最小值，最大值，25%分位数，75%分位数。
+print(power.describe())
+
+power["Global_active_power"].plot()
+plt.show()
